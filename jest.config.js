cf58b135f45fa42980fa1014e5f9266eb09432fd
Yann Isabel <isabel.yann@gmail.com>
2023-05-04 19:20:50 +0200

    jest config + more tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,26 @@
 module.exports = {
   roots: ["<rootDir>/src"],
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  coverageReporters: [
+    'json-summary', 'lcov'
+  ],
   coverageDirectory: './coverage',
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*?(.)types.{ts,tsx}',
+    '!**/*?(.)stories.{ts,tsx}',
+    '!**/*.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 28.57,
+      functions: 15.15,
+      lines: 7.76,
+      statements: 7.76
+    }
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
   testEnvironment: 'jsdom',
   fakeTimers: {
     enableGlobally: true,
