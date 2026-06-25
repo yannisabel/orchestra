6d91fa46fd83b67c7bdbd1580fb7b08bc0c48eec
yannisabel <yannisabel@users.noreply.github.com>
2026-06-25 10:47:11 +0000

    deploy: 60d8bd53bf2d5f1c586311eb57ab322a4224f65a

---
+++ assets/formatter-L2YTDE3Q-gzuBYBPI.js
@@ -0,0 +1,1 @@
+import{d as m,a as o}from"./blocks-CD49I05N.js";import{b as t}from"./iframe-CTWoi0RR.js";import"./preload-helper-PPVm8Dsz.js";import"./react-DkC3XXtW.js";import"./react-dom-CPA3uLtk.js";import"./react/jsx-runtime-DldvXD6N.js";import"./lit-X1EjRepF.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
