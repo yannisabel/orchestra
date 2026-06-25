bdf583e21c6b8d3f0ea52ac682cdce958f5ecc8c
yannisabel <yannisabel@users.noreply.github.com>
2026-06-25 09:56:18 +0000

    deploy: 22d34be5b8f60e730adf758ea216eca3264a04ad

---
+++ stencil-migration/assets/formatter-L2YTDE3Q-B3MGWTWz.js
@@ -0,0 +1,1 @@
+import{d as m,a as o}from"./blocks-CRA5L1Lm.js";import{b as t}from"./iframe-dIStHrxV.js";import"./preload-helper-BhJMTVfQ.js";import"./react-DkC3XXtW.js";import"./react-dom-CPA3uLtk.js";import"./react/jsx-runtime-DldvXD6N.js";import"./lit-X1EjRepF.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
