4474159dc4c92de6517cb09ab8b1dd204f86eb9d
yannisabel <yannisabel@users.noreply.github.com>
2026-07-09 13:13:43 +0000

    deploy: 34d76eb4fbdb5006c77fdd8792ccffb8c5966c3d

---
+++ branches/update-design-tokens-logic/assets/icon-CqRcRVX8.js
@@ -0,0 +1,47 @@
+import{u as t,j as n,f as i,T as c,C as s,h as a}from"./blocks-BWuEvnL2.js";import{I as l,D as d,C as h}from"./icon.stories-k6TU1juQ.js";import"./iframe-TTOnA9n1.js";import"./preload-helper-PPVm8Dsz.js";import"./react-DkC3XXtW.js";import"./react-dom-CPA3uLtk.js";import"./react/jsx-runtime-DldvXD6N.js";import"./lit-X1EjRepF.js";const p="# orchestra-icon\n\n\n\n<!-- Auto Generated Below -->\n\n\n## Properties\n\n| Property            | Attribute | Description                                                                            | Type     | Default             |\n| ------------------- | --------- | -------------------------------------------------------------------------------------- | -------- | ------------------- |\n| `fill`              | `fill`    | Taking the currentcolor (inherited color of the font) by default, except if specified. | `string` | `'currentcolor'`    |\n| `library`           | `library` | The name of the icon library to use. Defaults to 'orchestra-icons'.                    | `string` | `'orchestra-icons'` |\n| `name` _(required)_ | `name`    | The name of the icon to display. Resolves using registered icon libraries.             | `string` | `undefined`         |\n| `size`              | `size`    | Taking the size of the parent element by default, except if specified.                 | `string` | `'100%'`            |\n\n\n## Dependencies\n\n### Used by\n\n - [orchestra-button](../button)\n\n### Graph\n```mermaid\ngraph TD;\n  orchestra-button --> orchestra-icon\n  style orchestra-icon fill:#f9f,stroke:#333,stroke-width:4px\n```\n\n----------------------------------------------\n\n\n";function o(r){const e={code:"code",h2:"h2",h3:"h3",h4:"h4",p:"p",pre:"pre",...t(),...r.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:l}),`
+`,n.jsx(c,{}),`
+`,n.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,n.jsx(e.h3,{id:"default-library-core",children:"Default Library (Core)"}),`
+`,n.jsx(s,{of:d}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-html",children:`<orchestra-icon name="checked" fill="currentcolor" size="60px"></orchestra-icon>
+`})}),`
+`,n.jsx(e.h3,{id:"custom-library",children:"Custom Library"}),`
+`,n.jsx(s,{of:h}),`
+`,n.jsx(e.h4,{id:"with-hardcoded-svgs",children:"With Hardcoded SVGs"}),`
+`,n.jsx(e.p,{children:"To use a custom icon library with hardcoded SVGs, register it before rendering:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-typescript",children:`import { registerIconLibrary } from '@orchestra-design-system/core'
+
+registerIconLibrary('myIcons', {
+  resolver: (name) => {
+    const icons: Record<string, string> = {
+      star: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">...</svg>',
+      heart:
+        '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">...</svg>',
+    }
+    return icons[name] ?? ''
+  },
+})
+`})}),`
+`,n.jsx(e.h4,{id:"from-an-external-package",children:"From an External Package"}),`
+`,n.jsx(e.p,{children:"For production use, import icons from an external package:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-typescript",children:`import { registerIconLibrary } from '@orchestra-design-system/core'
+import * as Icons from '@your-org/icons-library'
+
+registerIconLibrary('externalIcons', {
+  resolver: (name) => {
+    const iconExport = (Icons as Record<string, any>)[name]
+    return typeof iconExport === 'string' ? iconExport : ''
+  },
+})
+`})}),`
+`,n.jsx(e.p,{children:"Then use it:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-html",children:`<orchestra-icon
+  name="star"
+  library="externalIcons"
+  fill="red"
+  size="24px"
+></orchestra-icon>
+`})}),`
+`,n.jsx(e.h2,{id:"component-properties",children:"Component Properties"}),`
+`,n.jsx("div",{className:"stencil-props",children:n.jsx(a,{children:p})})]})}function w(r={}){const{wrapper:e}={...t(),...r.components};return e?n.jsx(e,{...r,children:n.jsx(o,{...r})}):o(r)}export{w as default};
+//# sourceMappingURL=icon-CqRcRVX8.js.map
