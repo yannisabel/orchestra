2a568c407376e13d03de3e5557a11ca588d6a51e
yannisabel <yannisabel@users.noreply.github.com>
2026-07-29 18:13:48 +0000

    deploy: dcd3b0deb7fa0c03886ce3194a8afaae7bc30751

---
+++ branches/update-design-tokens/assets/formatter-L2YTDE3Q-CdA-f5Y5.js
@@ -0,0 +1,2 @@
+import{d as m,a as o}from"./blocks-DP7nT8mb.js";import{b as t}from"./iframe-RjTo5Q6E.js";import"./preload-helper-PPVm8Dsz.js";import"./react-DkC3XXtW.js";import"./react-dom-CPA3uLtk.js";import"./react/jsx-runtime-DldvXD6N.js";import"./lit-X1EjRepF.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
+//# sourceMappingURL=formatter-L2YTDE3Q-CdA-f5Y5.js.map
