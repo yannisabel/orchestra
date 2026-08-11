63e5a0dd56bedc2d7f8de5ba009ad38378208fdc
yannisabel <yannisabel@users.noreply.github.com>
2026-08-11 16:23:09 +0000

    deploy: 51af44ac722a1bb80f51d737760f326a91aa3059

---
+++ branches/feature-add-checkbox-component/assets/formatter-L2YTDE3Q-CitNG36e.js
@@ -0,0 +1,2 @@
+import{d as m,a as o}from"./blocks-C92yrjpZ.js";import{b as t}from"./iframe-DXL_HCC5.js";import"./preload-helper-PPVm8Dsz.js";import"./react-DkC3XXtW.js";import"./react-dom-CPA3uLtk.js";import"./react/jsx-runtime-DldvXD6N.js";import"./lit-X1EjRepF.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
+//# sourceMappingURL=formatter-L2YTDE3Q-CitNG36e.js.map
